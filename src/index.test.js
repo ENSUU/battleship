ea7fc36ffd5dc0fa4ade5107b71c303c1b95a541
ENSUU <eunsu.dk.kim@gmail.com>
2023-11-17 15:25:52 -0500

    Add general test cases for the Gameboard class
    Created unit tests for the Gameboard class that needs to be implemented.

--- src/index.test.js
+++ src/index.test.js
@@ -30,46 +30,46 @@ test('.populateCoords method working properly', () => {
 })
 
 // Gameboard Tests
-// test('Adding ship to the gameboard', () => {
-//     const testShip = new Ship(1, [2,2], 'top'); 
-//     const testBoard = new Gameboard(); 
+test('Adding ship to the gameboard', () => {
+    const testShip = new Ship(1, [2,2], 'top'); 
+    const testBoard = new Gameboard(); 
 
-//     testBoard.add(testShip); 
-//     expect(testBoard.ships).toContain(testShip);
-// }); 
+    testBoard.add(testShip); 
+    expect(testBoard.ships).toContain(testShip);
+}); 
 
-// test('Place ship at coordinates', () => {
-//     // For this to work, constructor needs to be updated. 
-//     // On object construction, need to pass in ship's length, staring coords, and direction from starting coord (top, left, right, bottom);
-//     const testShip = new Ship(3, [0, 0], 'right');
-//     const testBoard = new Gameboard(); 
+test('Place ship at coordinates', () => {
+    // For this to work, constructor needs to be updated. 
+    // On object construction, need to pass in ship's length, staring coords, and direction from starting coord (top, left, right, bottom);
+    const testShip = new Ship(3, [0, 0], 'right');
+    const testBoard = new Gameboard(); 
 
-//     expect(testShip.coords).toBe([[0,0], [1,1], [2,2]]); 
-// }); 
+    expect(testShip.coords).toBe([[0,0], [1,1], [2,2]]); 
+}); 
 
-// test("Gameboard's receiveAttack function working properly", () => {
-//     const testShip = new Ship(3, [0,0], 'top'); 
-//     const testBoard = new Gameboard(); 
-//     // The game board will be a 10 x 10 2D array. 
+test("Gameboard's receiveAttack function working properly", () => {
+    const testShip = new Ship(3, [0,0], 'top'); 
+    const testBoard = new Gameboard(); 
+    // The game board will be a 10 x 10 2D array. 
 
-//     // Testing receiveAttack function of gameboard 
-//     // - Case 1: Valid coordinate -> Increment ship's hit count by 1. 
-//     expect(testBoard.receiveAttack([0,0])).toBe(true); 
-//     expect(testShip.hitCount).toBe(1); 
+    // Testing receiveAttack function of gameboard 
+    // - Case 1: Valid coordinate -> Increment ship's hit count by 1. 
+    expect(testBoard.receiveAttack([0,0])).toBe(true); 
+    expect(testShip.hitCount).toBe(1); 
 
-//     // - Case 2: Invalid coordinate -> Record missed attack coordinate. 
-//     // Probably need an attribute for GameBoard to keep track of this. 
-//     expect(testBoard.receiveAttack([5,2])).toBe(false); 
-//     expect(testBoard.missedShots).toBe([[5,2]]); 
-// }); 
+    // - Case 2: Invalid coordinate -> Record missed attack coordinate. 
+    // Probably need an attribute for GameBoard to keep track of this. 
+    expect(testBoard.receiveAttack([5,2])).toBe(false); 
+    expect(testBoard.missedShots).toBe([[5,2]]); 
+}); 
 
-// test('Checking if all ships on board sank', () => {
-//     const testBoard = new Gameboard(); 
-//     const testShip = new Ship(1, [2,2], 'top'); 
+test('Checking if all ships on board sank', () => {
+    const testBoard = new Gameboard(); 
+    const testShip = new Ship(1, [2,2], 'top'); 
 
-//     testBoard.add(testShip); 
-//     expect(testBoard.isSunk).toBe(false); 
+    testBoard.add(testShip); 
+    expect(testBoard.isSunk).toBe(false); 
 
-//     testBoard.receiveAttack([2,2]); 
-//     expect(testBoard.isSunk).toBe(true); 
-// }); 
+    testBoard.receiveAttack([2,2]); 
+    expect(testBoard.isSunk).toBe(true); 
+}); 
