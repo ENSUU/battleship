7df9651a3d01ca1f51e9aec0f835ea28ab5d43b3
ENSUU <eunsu.dk.kim@gmail.com>
2023-11-17 15:22:03 -0500

    Update the Ship class to have attribute .coords instead of .startCoord
    Instead of .startCoord, replaced with .coords which is an array
    containing all the grid coordinates the ship occupies on the board.
    When a Ship object is created, .coords will be populated depending on
    the specified direction.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ class Ship {
         this.length = length; 
         this.hitCount = 0; 
         this.sunkStatus = false; 
-        this.startCoord = startCoord; 
+        this.coords = [startCoord];
         this.direction = direction; 
+        this.populateCoords();
     }
 
     hit() {
@@ -17,12 +18,42 @@ class Ship {
     isSunk() {
         return this.sunkStatus; 
     }
+
+    populateCoords() {
+        // If the length of the ship is 1, it occupies only one cell. The starting coordinate is the ship's only coordinate. 
+        if (this.length < 2) return; 
+
+        // Depending on the direction specified, the ship's offset will either increment or decrement the x or y coordinate. 
+        const offsets = [[0, 1], [-1, 0], [1, 0], [0, -1]]; 
+
+        // Determining the ship's offset depending on the direction specified. 
+        let shipOffset; 
+        if (this.direction === 'top') shipOffset = offsets[0]; 
+        else if (this.direction === 'left') shipOffset = offsets[1]; 
+        else if (this.direction === 'right') shipOffset = offsets[2]; 
+        else shipOffset = offsets[3]; 
+
+        // Populating the ship's .coords attribute with the cells the ship occupies. 
+        let curX = this.coords[0][0]; 
+        let curY = this.coords[0][1]; 
+
+        for (let i = 0; i < this.length - 1; i++) {
+            let newX = curX + shipOffset[0]; 
+            let newY = curY + shipOffset[1];
+            this.coords.push([newX, newY]); 
+            curX = newX; 
+            curY = newY; 
+        }
+    }
 }; 
 
-class GameBoard {
+class Gameboard {
     constructor() {
-
+        this.grid = Array.from(Array(10), () => new Array(10)); 
+        this.ships = {}; 
+        this.isSunk = false; 
     }
 }
 
-module.exports = Ship; 
+
+module.exports = {Ship, Gameboard}; 
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const Ship = require('./index'); 
+const {Ship, Gameboard} = require('./index'); 
 
 // Ship Tests
 test('Accessing Ship object .length and .sunkStatus property', () => {
@@ -8,7 +8,7 @@ test('Accessing Ship object .length and .sunkStatus property', () => {
     expect(testShip.sunkStatus).toBe(false); 
 }); 
 
-test('Ship object .hit and .isSunk functions working', () => {
+test('Ship object .hit and .is`Sunk functions working', () => {
     const testShip = new Ship(2, [2,2], 'bottom'); 
     testShip.hit(); 
     expect(testShip.hitCount).toBe(1); 
@@ -18,11 +18,58 @@ test('Ship object .hit and .isSunk functions working', () => {
     expect(testShip.sunkStatus).toBe(true); 
 }); 
 
-// Gameboard Tests
-// - Gameboard is 10 x 10. 
-test('Place ship at coordinates', () => {
-    // For this to work, constructor needs to be updated. 
-    // On object construction, need to pass in ship's length, staring coords, and direction from starting coord (top, left, right, bottom);
-    const testShip = new Ship(3, [0, 0], 'right');
-    expect(testShip.coords).toBe([[0,0], [1,1], [2,2]]); 
+test('.populateCoords method working properly', () => {
+    const testShip = new Ship(3, [2,2], 'top'); 
+    expect(testShip.coords).toStrictEqual([[2,2], [2,3], [2,4]]); 
+
+    const testShipTwo = new Ship(2, [5,5], 'bottom'); 
+    expect(testShipTwo.coords).toStrictEqual([[5,5], [5,4]]);
+
+    const testShipThree = new Ship(1, [0,0], 'right'); 
+    expect(testShipThree.coords).toStrictEqual([[0,0]]);
 })
+
+// Gameboard Tests
+// test('Adding ship to the gameboard', () => {
+//     const testShip = new Ship(1, [2,2], 'top'); 
+//     const testBoard = new Gameboard(); 
+
+//     testBoard.add(testShip); 
+//     expect(testBoard.ships).toContain(testShip);
+// }); 
+
+// test('Place ship at coordinates', () => {
+//     // For this to work, constructor needs to be updated. 
+//     // On object construction, need to pass in ship's length, staring coords, and direction from starting coord (top, left, right, bottom);
+//     const testShip = new Ship(3, [0, 0], 'right');
+//     const testBoard = new Gameboard(); 
+
+//     expect(testShip.coords).toBe([[0,0], [1,1], [2,2]]); 
+// }); 
+
+// test("Gameboard's receiveAttack function working properly", () => {
+//     const testShip = new Ship(3, [0,0], 'top'); 
+//     const testBoard = new Gameboard(); 
+//     // The game board will be a 10 x 10 2D array. 
+
+//     // Testing receiveAttack function of gameboard 
+//     // - Case 1: Valid coordinate -> Increment ship's hit count by 1. 
+//     expect(testBoard.receiveAttack([0,0])).toBe(true); 
+//     expect(testShip.hitCount).toBe(1); 
+
+//     // - Case 2: Invalid coordinate -> Record missed attack coordinate. 
+//     // Probably need an attribute for GameBoard to keep track of this. 
+//     expect(testBoard.receiveAttack([5,2])).toBe(false); 
+//     expect(testBoard.missedShots).toBe([[5,2]]); 
+// }); 
+
+// test('Checking if all ships on board sank', () => {
+//     const testBoard = new Gameboard(); 
+//     const testShip = new Ship(1, [2,2], 'top'); 
+
+//     testBoard.add(testShip); 
+//     expect(testBoard.isSunk).toBe(false); 
+
+//     testBoard.receiveAttack([2,2]); 
+//     expect(testBoard.isSunk).toBe(true); 
+// }); 
